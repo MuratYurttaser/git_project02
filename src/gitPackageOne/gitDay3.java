a68c0bf6b14a4326410d558ba08df207da3bd77d
Murat Yurttaser <murat@Admins-MBP-6.fios-router.home>
2020-03-15 15:05:10 -0400

    second commit from local repo

--- src/gitPackageOne/gitDay3.java
+++ src/gitPackageOne/gitDay3.java
@@ -1,5 +1,20 @@
 package gitPackageOne;
 
 public class gitDay3 {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("My first changes on local master");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 
 }
